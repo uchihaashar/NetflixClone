cd13f4400bdac100c8ef93bcb047ec5335956525
uchihaashar <mohdasharshakeel@gmail.com>
2025-01-28 11:29:29 +0530

    deploy ready

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
-						you
+						Ashar
 					</a>
 					. The source code is available on{" "}
 					<a
